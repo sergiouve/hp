e60f3548c4809e606c340a262510b750c16d69d0
Sergio Uve <sergiouve@gmail.com>
2019-03-25 17:23:49 +0100

    Add scaffold for pretty printer

--- printers/pretty.js
+++ printers/pretty.js
@@ -1,5 +1,13 @@
 const pretty = output => {
-    console.log(output);
+    const percent = output.percent;
+    let bar = '[====================]';
+
+    // TODO
+    const toRemove = 2 - output.percent;
+
+    const final = `${bar} ${parseFloat(percent * 100).toFixed(2)} %`
+
+    console.log(final);
 };
 
 module.exports = pretty;
